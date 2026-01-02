aaed8aae9c70cb9a78812ea9c6558537ba1f2b03
Khushbu <guptakhushu2106@gmail.com>
2026-01-02 12:55:21 +0530

    feat:built services , aboout with styling

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -39,7 +39,7 @@ const type = () => {
         }
     }
     else {
-        typingText.textContent = currentWord.substring(0, currentWord - 1);
+        typingText.textContent = currentWord.substring(0, charIndex - 1);
         charIndex--;
 
         if (charIndex === 0) {
@@ -84,6 +84,104 @@ navlinks.forEach((link) => {
             else {
                 tab.classList.remove("active");
             }
-        })
-    })
-})
+        });
+        if (tabName === 'services') {
+            const servicesList = [{
+                id: 1,
+                icon: "ph-code",
+                text: "Website development",
+                para: "I build responsive and modern websited using the latest technologies like HTML,CSS,JavaScript etc",
+            },
+            {
+                id: 2,
+                icon: "",
+                text: "",
+                para: "",
+            },
+            {
+                id: 3,
+                icon: "",
+                text: "",
+                para: "",
+            },
+            ];
+            const services = document.getElementsByClassName("service-list");
+
+            const innerContent = servicesList.map((l) => {
+                return `
+                <div class="box" key=${l?.id}>
+                        <div class="head-icons">
+                            <i class="ph ${l?.icon}"></i>
+                            <span>
+                                <i class="ph ph-arrow-down"></i>
+                            </span>
+                        </div>
+                        <h3>${l?.text}</h3>
+                        <span id="spacer">
+                            
+                        </span>
+                        <p>${l?.para}</p>
+                    </div>
+                `;
+            }).join(" ");
+
+            Array.from(services).forEach((ele) => {
+                ele.innerHTML = innerContent;
+            });
+
+        }
+        toggle.checked = false;
+    });
+});
+
+const aboutTabs = document.querySelectorAll(".tab");
+
+const aboutContent = document.querySelectorAll(".tab-content")
+
+// document.addEventListener('DOMContentLoaded' ,()=>{
+//     if(aboutTabs){
+//         aboutTabs[0].click
+//     }
+// });
+
+aboutTabs.forEach((tab) => {
+    tab.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        aboutTabs.forEach((a) => {
+            a, classList.remove("active")
+        });
+        tab.classList.add("active");
+
+        aboutContent.forEach((c) => {
+            c.classList.remove("active");
+        });
+
+        const activeTab = tab.dataset.section;
+        document.getElementById(activeTab).classList.add("active");
+
+        if (activeTab === 'experience') {
+            const experience = document.querySelector(".experience-list");
+
+            const experienceList = [{
+                id: 1,
+                data: "octomber 2025",
+                position: 'Frontend Developer',
+                details: "I worked on building responsive websites using HTML, CSS, and JavaScript."
+            },
+            {
+                id: 2,
+                data: "octomber 2025",
+                position: 'Frontend Developer',
+                details: "I worked on building responsive websites using HTML, CSS, and JavaScript."
+            }
+            ];
+
+            const experienceContent = experienceList.map((ele)=>{
+                return `
+                
+                `
+            })
+        }
+    });
+});
